8f73976227a9b8895e3d9ef489d06a01e7f8492b
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-11 19:48:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -394,11 +394,13 @@ def player_with_longest_name
 
                 #end
 
+                return longest_name_size
+
 
 
             end
 
-            return longest_name_size
+            
         end
     end
 end
