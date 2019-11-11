859a1b2b326539b07cde71820b110b49c3a1492b
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-11 20:49:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -399,9 +399,10 @@ game_hash.each do |team, team_data|
         if key == :players
             value.each do |player|
                 if player[:player_name] == player_with_longest_name
-      
-
-
+    
+                  if player[:steals] > 15
+                  return true
+                end
                     #binding.pry
 
 
