7a3d66b5dad645a7e65458927b7c2b95cef57de8
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-11 20:15:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -380,9 +380,9 @@ def player_with_longest_name
 
                 value.each do |player|
                     length_of_name.push(player[:player_name].length-1)
-                    return length_of_name
                 end
 
+return length_of_name
 
 
 
