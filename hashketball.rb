df6a1afa01747d8d701944b9e72d0822804e6cd1
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-11 19:47:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -385,15 +385,16 @@ def player_with_longest_name
                 length_of_name = length_of_name.sort
                 longest_name_size = length_of_name[length_of_name.length-1]
 
-                  binding.pry
-
                 #value.each do |player|
                   #if player[:player_name].length == longest_name_size
                     #player_name = player[:player_name]
                     #return player_name
                     #binding.pry
                   #end
+                  
                 #end
+
+                return longest_name_size
 
             end
         end
