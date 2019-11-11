e48462b2f7561283519d9b2dcb76e9a2aba819c3
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-11 18:00:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -293,7 +293,7 @@ def most_points_scored
         team_data.each do |key, value|
             if key == :players
                 value.each do |player|
-                    points.push(player[:points].to_i)
+                    points.push(player[:points])
                 end
 
                 points = points.sort
