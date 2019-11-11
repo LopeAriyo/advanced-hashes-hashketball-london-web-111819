c53dbc6cb5b7ae3d342fbbf2e5cb6967c3aeea6d
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-11 16:52:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -231,13 +231,13 @@ def player_stats(player_name)
                   value.each do |player|
                       if player[:player_name] == player_name
 
-                        
 
-                        
+
+
                         #stats = player.delete_if do |k,v|
                         #k  == :player_name
                         #end
-                      
+
                       end
                   end
               end
